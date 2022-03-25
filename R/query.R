5e89d2573596136ca6c99dd0818a861266bdb56a
Anh Nguyet Vu <anngvu@gmail.com>
2022-03-25 12:35:31 -0600

    Add schema query utils

--- R/query.R
+++ R/query.R
@@ -16,3 +16,43 @@ table_query <- function(table_id, columns = "*", includeRowIdAndRowVersion = F) 
     readr::read_csv(na=character())
   table
 }
+
+#' Consult schema about max string length 
+#' 
+#' Utility to query the schema regarding the max string length for a key
+#' based on current valid values. If the key values are not constrained (free-text), 
+#' a default string length of 100 is returned. If not in schema, returns NA.
+#' TO DO: related fun to consult schema about type (integer, string, stringlist, etc.) 
+schema_max_str_len <- function(key,
+                               schema_url = 'https://raw.githubusercontent.com/nf-osi/nf-metadata-dictionary/main/NF.jsonld',
+                               parent_context = 'bts',
+                               default = 100) {
+  
+  id <- key_label_to_id(key, prefixed = FALSE)
+  if(!length(id)) return(NA_integer_)
+  vals <- get_valid_values_from_json_schema(schema_url, parent_name = id)
+  if(!length(vals)) return(default)
+  chars <- sapply(vals, nchar)
+  return(max(chars))
+}
+
+#' Query for schema key id given label
+#' 
+#' Utility to translate label to id using a schematic-generated schema.
+#' @inheritParams get_valid_values_from_json_schema
+#' @param label The term label, a.k.a display name.
+#' @param prefixed Boolean to indicate whether to include namespace prefix or return bare ID. Defaults to `TRUE`.
+#' @return The id if found, such as "bts:MyID", otherwise an empty character vector.
+#' @export
+key_label_to_id <- function(label,
+                            prefixed = TRUE,
+                            schema_url = "https://raw.githubusercontent.com/nf-osi/nf-metadata-dictionary/main/NF.jsonld"
+) {
+  
+  id <- jsonlite::fromJSON(schema_url) %>%
+    purrr::pluck("@graph") %>%
+    dplyr::filter(`sms:displayName` == label) %>%
+    dplyr::pull(`@id`)
+  
+  if(prefixed) id else gsub("^.*:", "", id) # still returns empty character if not found
+}
