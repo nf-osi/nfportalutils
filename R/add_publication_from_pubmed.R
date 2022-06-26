6006516a136358c1665475aa3d072333c80ef670
Anh Nguyet Vu <anngvu@gmail.com>
2022-06-26 10:47:14 -0600

    Streamline from_pubmed even more, update params docs

--- R/add_publication_from_pubmed.R
+++ R/add_publication_from_pubmed.R
@@ -1,14 +1,15 @@
 #' Add a publication to the publication table
 #' 
 #' Requires that the publication be in PubMed to derive metadata such as authors, title, etc.
-#' Fields `disease_focus` and `manifestation` can be supplemented manually by the curator. 
-#' The `study_id` is used to join with study table to create view with consistent `studyName` and `fundingAgency`. 
+#' Fields `disease_focus` and `manifestation` also derived but can be _supplemented_ by the curator as args. 
+#' The `study_id` is used to get consistent `studyName` and `fundingAgency` from study table without manual input. 
 #' 
-#' @param publication_table_id The synapse id of the portal publication table. Must have write access.
-#' @param pmid The PubMed ID (*not* PMCID) of the publication to be added.
-#' @param study_id The synapse id(s) of the study that are associated with the publication. 
+#' @param pmid PubMed ID (*not* PMCID) of the publication to be added.
+#' @param study_id Synapse id(s) of the study that are associated with the publication. 
 #' @param disease_focus The disease focus(s) that are associated with the publication.
 #' @param manifestation The manifestation(s) that are associated with the publication.
+#' @param publication_table_id Synapse id of the portal publication table. Must have write access.
+#' @param study_table_id Synapse id of the portal study table. Need read access.
 #' @param dry_run Default = TRUE. Skips upload to table and instead prints formatted publication metadata.
 #' @return If dry_run == T, returns publication metadata to be added.
 #' @examples 
@@ -19,8 +20,7 @@
 #'                disease_focus = c("Neurofibromatosis"),
 #'                manifestation = c("Meningioma"),
 #'                publication_table_id = "syn16857542",
-#'                study_table_id = "",
-#'                dry_run = T)
+#'                study_table_id = "")
 #'}
 #' @export
 add_publication_from_pubmed <- function(pmid, 
@@ -55,22 +55,19 @@ add_publication_from_pubmed <- function(pmid,
 
 #' Get publication metadata from PubMed
 #' 
-#' Get PMID meta as table with `title`, `journal`, `author`, `year`, `pmid`, `doi` and, 
-#' optionally, `keywords`. If PMID is not found, returns `NA`.
+#' If PMID found, return meta as table w/ `title` `journal` `author` `year` `pmid` `doi` `keywords`, else `NA`.
 #' 
-#' @param pmid PubMed id number.
-#' @param with_keywords Whether to include keywords.
+#' @param pmid PubMed id.
 #' @export
-from_pubmed <- function(pmid, with_keywords = TRUE) {
+from_pubmed <- function(pmid) {
   
   res <- easyPubMed::get_pubmed_ids(pmid) 
   if(res$Count == 0) { 
-    message("Nothing found for PMID")
+    message(glue::glue("Nothing found for PMID:{pmid}"))
     return(NA) # Return NA early if no records found 
   }
-  res <- easyPubMed::fetch_pubmed_data(pmids, format = "xml", retmax = 1)
-  
-  p <- xml2::as_list(xml2::read_xml(res[[1]]))
+  p <- easyPubMed::fetch_pubmed_data(res, format = "xml", retmax = 1) %>% 
+    `[[`(1) %>% xml2::read_xml() %>% xml2::as_list()
   
   authors <- mapply(function(author) paste(author$ForeName, author$LastName), 
                     p$PubmedArticleSet$PubmedArticle$MedlineCitation$Article$AuthorList) 
@@ -78,14 +75,11 @@ from_pubmed <- function(pmid, with_keywords = TRUE) {
   title <- p$PubmedArticleSet$PubmedArticle$MedlineCitation$Article$ArticleTitle[[1]]
   doi <- paste0("https://www.doi.org", p$PubmedArticleSet$PubmedArticle$MedlineCitation$Article$ELocationID[[1]])
   year <- p$PubmedArticleSet$PubmedArticle$MedlineCitation$Article$Journal$JournalIssue$PubDate$Year[[1]]
-
-  record <- data.frame(title = title, journal = journal, author = I(list(authors)),
-                       year = year, pmid = pmid, doi = doi)
+  keywords <- unique(unlist(p$PubmedArticleSet$PubmedArticle$MedlineCitation$MeshHeadingList, 
+                                 p$PubmedArticleSet$PubmedArticle$MedlineCitation$KeywordList))
   
-  if(with_keywords) {
-    record$keywords <- list(unique(unlist(p$PubmedArticleSet$PubmedArticle$MedlineCitation$MeshHeadingList, 
-                  p$PubmedArticleSet$PubmedArticle$MedlineCitation$KeywordList)))
-  }
+  record <- data.frame(title = title, journal = journal, author = I(list(authors)),
+                       year = year, pmid = pmid, doi = doi, keywords = I(list(keywords)))
   return(record)
 }
 
