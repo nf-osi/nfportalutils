d9c201c7321075ea1f88e1069f27988024178b96
Anh Nguyet Vu <anngvu@gmail.com>
2022-06-27 11:34:05 -0600

    Modify add_publication for batch use cases (WIP)

--- R/add_publication_from_pubmed.R
+++ R/add_publication_from_pubmed.R
@@ -1,7 +1,7 @@
 #' Add a publication to the publication table
 #' 
-#' Requires that the publication be in PubMed to derive metadata such as authors, title, etc.
-#' Fields `disease_focus` and `manifestation` also derived but can be _supplemented_ by the curator as args. 
+#' Requires that the publication be in PubMed to auto-derive metadata such as authors, title, etc.
+#' In contrast, `disease_focus` and `manifestation` need to be supplemented by the curator. 
 #' The `study_id` is used to get consistent `studyName` and `fundingAgency` from study table without manual input. 
 #' 
 #' @param pmid PubMed ID (*not* PMCID) of the publication to be added.
@@ -23,35 +23,52 @@
 #'                study_table_id = "")
 #'}
 #' @export
-add_publication_from_pubmed <- function(pmid, 
-                                        study_id, 
-                                        disease_focus, 
-                                        manifestation, 
-                                        publication_table_id,
-                                        study_table_id,
-                                        dry_run = T) {
-  .check_login()
+add_publication_from_pubmed <- .add_publication_from_pubmed()
 
-  # Query only for data needed, i.e. PMID to check non-dup; don't need all data to add new row
-  pmids <- table_query(publication_table_id, "pmid") %>% unlist(use.names = F)
-  
-  if(pmid %in% pmids) {
-      message(glue::glue("PMID:{pmid} already exists in destination table!")) 
-  } else {
-    record <- from_pubmed(pmid) 
-    if(is.na(record)) return()
+#' Higher-level fun to generate `add_publication_from_pubmed` util for one-off usage (default) or optimized for batch processing.
+#' @param batch If a non-zero batch size, turns on batch mode; defaults to no-batch.
+#' @param cache Whether to cache some results, which is default if `batch`.
+#' @keywords internal
+.add_publication_from_pubmed <- function(batch = 0L, cache = batch) {
+  pmids <- NULL
+  new_data <- NULL
+  function(pmid, study_id, disease_focus, manifestation, 
+           publication_table_id, study_table_id, dry_run = T) {
     
-    new_row <- table_join(record, study_table_id, studyId)  
-    new_row <- as_table_schema(new_row, table = publication_table_id)
-    if(!dry_run) {
-      stored <- .syn$store(new_row)
-      message(glue::glue('PMID:{pmid} added!'))
-    } else{
-      new_row
+    .check_login()
+    
+    # Query only for data needed, i.e. PMID to check non-dup; result can be cached
+    if(is.null(pmids)) {
+      pmids <- table_query(publication_table_id, "pmid") %>% unlist(use.names = F)
+      if(cache) pmids <<- pmids
+    }
+    
+    if(pmid %in% pmids) {
+      message(glue::glue("PMID:{pmid} already exists in destination table!")) 
+    } else {
+      record <- from_pubmed(pmid, nf_keywords = TRUE) 
+      if(is.na(record)) return()
+      
+      study_id_set <- glue::glue_collapse(glue::single_quote(study_id), sep = ", ")
+      study <- .syn$tableQuery(glue::glue("SELECT studyId, studyName, fundingAgency FROM {study_table_id} WHERE studyId IN ({study_id_query})"))$asDataFrame()
+      record <- cbind(record, studyId = I(list(study$studyId)), studyName = I(list(study$studyName)), fundingAgency = I(list(study$fundingAgency)))
+      
+      # If batch mode, rbind and defer table schemafication until all records processed
+      if(batch) {
+        new_data <<- rbind(new_data, record)
+        if(nrow(new_data) == batch) new_data <- as_table_schema(new_data, publication_table_id)
+      } else {
+        new_data <- as_table_schema(record, publication_table_id)
+      }
+      if(!dry_run) {
+        new_data <- .syn$store(new_data)
+        message(glue::glue('PMID:{new_data$pmid} added!'))
+      } else{
+        new_data
+      }
     }
   }
 }
-
 
 #' Get publication metadata from PubMed
 #' 
@@ -82,4 +99,5 @@ from_pubmed <- function(pmid) {
                        year = year, pmid = pmid, doi = doi, keywords = I(list(keywords)))
   return(record)
 }
+
 
