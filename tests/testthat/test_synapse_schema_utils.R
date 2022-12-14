d3a40d6cf40686f2d36c2f69fdb1b540c3fbb127
Anh Nguyet Vu <anngvu@gmail.com>
2022-12-13 19:47:41 -0700

    Add some of my favorite tests

---
+++ tests/testthat/test_synapse_schema_utils.R
@@ -0,0 +1,52 @@
+# Test fixture is table syn49378540 on Synapse, with schema that includes INTEGER and STRING and STRING_LIST columns with character size and character + list size limits.
+# These tests mainly focus on the character and list validation and JSON encoding functionality that `as_table_schema` provides
+# in order to successfully append data conforming to an existing Synapse schema
+
+test_that("`as_table_schema` works as expected for data that can be fit into schema without issue", {
+  test_fixture <- "syn49378540"
+  test_data <- data.frame(Movie = "The Sound of Music",
+                          Year = 1965,
+                          Favorites = list("raindrops","whiskers", "kettles"))
+  test_data_storable <- as_table_schema(test_data, schema = test_fixture)
+  testthat::expect_no_error(.syn$store(test_data_storable))
+  
+})
+
+test_that("`as_table_schema` errors for data with missing column", {
+  test_fixture <- "syn49378540"
+  test_data <- data.frame(Movie = "The Sound of Music",
+                          Year = 1965)
+  testthat::expect_error(as_table_schema(test_data, schema = test_fixture))
+  
+})
+
+
+test_that("`as_table_schema` errors for data that exceeds list length as specified in schema for LIST column and truncation is not allowed", {
+  test_fixture <- "syn49378540"
+  test_data <- data.frame(Movie = "The Sound of Music",
+                          Year = 1965,
+                          Favorites = list("raindrops", "whiskers", "kettles", "mittens")) # exceeds list length of 3
+  testthat::expect_error(as_table_schema(test_data, schema = test_fixture, list_truncate = FALSE))
+  
+})
+
+
+test_that("`as_table_schema` returns result with warning for data that exceeds list length specified in schema for LIST column but truncated to fit", {
+  test_fixture <- "syn49378540"
+  test_data <- data.frame(Movie = "The Sound of Music",
+                          Year = 1965,
+                          Favorites = list("raindrops", "whiskers", "kettles", "mittens")) # exceeds list length of 3
+  testthat::expect_warning(as_table_schema(test_data, schema = test_fixture, list_truncate = TRUE))
+  
+})
+
+
+test_that("`as_table_schema` errors for data that exceeds character limits specified in schema for STRING_LIST column", {
+  test_fixture <- "syn49378540"
+  test_data <- data.frame(Movie = "The Sound of Music",
+                          Year = 1965,
+                          Favorites = list("raindrops on roses", "whiskers")) # exceeds character size of 15
+  testthat::expect_error(as_table_schema(test_data, schema = test_fixture))
+  
+})
+
