c7d3f6b739342562b80f225b8595ad7f14e1c472
Anh Nguyet Vu <anngvu@gmail.com>
2023-03-09 13:13:17 -0700

    Fix outdated test expectations

--- tests/testthat/test_json_schema_utils.R
+++ tests/testthat/test_json_schema_utils.R
@@ -41,7 +41,11 @@ test_that("Recursive lookup works with `get_dependency_from_json_schema`", {
          "specimenPreparationMethod",
          "tissue",
          "tumorType",
-         "entityId")
+         "entityId",
+         "transplantationType",
+         "transplantationRecipientSpecies",
+         "transplantationRecipientTissue",
+         "isXenograft")
   
   k_test <- get_dependency_from_json_schema(id = "bts:GenomicsAssayTemplate") 
   testthat::expect_equal(sort(k), sort(k_test))
