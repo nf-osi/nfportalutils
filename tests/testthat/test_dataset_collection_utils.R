cefa999261b4f2265c92a081bed9ce5419028e00
Anh Nguyet Vu <anngvu@gmail.com>
2023-07-02 09:44:38 -0600

    More unit tests

--- tests/testthat/test_dataset_collection_utils.R
+++ tests/testthat/test_dataset_collection_utils.R
@@ -21,7 +21,58 @@ test_that("Update helper for item collection works with combined 'replace' and '
   )
   
   testthat::expect_identical(update_items(current, update),
-                         expected)
+                             expected)
   
 })
 
+
+test_that("Update helper for item collection works with just 'replace' update type", {
+  
+  current <- list(
+    list(entityId = "syn1", versionNumber = 1L),
+    list(entityId = "syn2", versionNumber = 1L),
+    list(entityId = "syn3", versionNumber = 1L)
+  )
+  
+  update <- list(
+    list(entityId = "syn2", versionNumber = 2L),
+    list(entityId = "syn3", versionNumber = 2L)
+  )
+  
+  expected <- list(
+    list(entityId = "syn1", versionNumber = 1L),
+    list(entityId = "syn2", versionNumber = 2L),
+    list(entityId = "syn3", versionNumber = 2L)
+  )
+  
+  testthat::expect_identical(update_items(current, update),
+                             expected)
+  
+})
+
+
+test_that("Update helper for item collection works with just 'add' update type", {
+  
+  current <- list(
+    list(entityId = "syn1", versionNumber = 1L),
+    list(entityId = "syn2", versionNumber = 1L),
+    list(entityId = "syn3", versionNumber = 1L)
+  )
+  
+  update <- list(
+    list(entityId = "syn4", versionNumber = 2L),
+    list(entityId = "syn5", versionNumber = 2L)
+  )
+  
+  expected <- list(
+    list(entityId = "syn1", versionNumber = 1L),
+    list(entityId = "syn2", versionNumber = 1L),
+    list(entityId = "syn3", versionNumber = 1L),
+    list(entityId = "syn4", versionNumber = 2L),
+    list(entityId = "syn5", versionNumber = 2L)
+  )
+  
+  testthat::expect_identical(update_items(current, update),
+                             expected)
+  
+})
