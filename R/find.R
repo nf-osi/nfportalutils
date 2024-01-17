3f110ac6a657498e7ea37a24dc974406b4abe957
Anh Nguyet Vu <32753274+anngvu@users.noreply.github.com>
2024-01-17 12:02:48 -0700

    Revalidation workflow (#162)
    * Add helper finder
    
    * Add remanifest util
    
    * Refine development of remanifest
    
    * Add precheck/supplemental check util
    
    * Update docs
    
    * Update pkgdown index
    
    * Exports
    
    * Update expected errors and warnings
    
    * Expand handling of modern datasets elsewhere
    
    * Update output
    
    * Update exports, test message
    
    * Update docs
    
    * Add vignette

--- R/find.R
+++ R/find.R
@@ -1,15 +1,15 @@
 # Utils to help overcome nested folders
 
 #' Find in path
-#' 
+#'
 #' Get the Synapse id of an entity nested several folder layers deep without
 #' having to click through the UI or create a fileview as long as the structure/path is known.
-#' 
+#'
 #' @param scope Id of the container (project or folder) to begin search.
 #' @param path Path string in format "subdir1/subdir2/file.txt", where the last-level element will be the id returned.
 #' @export
 find_in <- function(scope, path) {
-  
+
   path_list <- strsplit(path, split = "/", fixed = TRUE)[[1]]
   here <- scope
   id <- NULL
@@ -28,11 +28,11 @@ find_in <- function(scope, path) {
 #' @param child_name Name of child entity.
 #' @export
 find_child <- function(child_name, parent) {
-  
+
   q <- .syn$getChildren(parent)
   child_id <- NULL
   repeat {
-    x <- reticulate::iter_next(q) 
+    x <- reticulate::iter_next(q)
     if(is.null(x) || x$name == child_name) {
       child_id <- x$id
       break
@@ -41,18 +41,37 @@ find_child <- function(child_name, parent) {
   child_id
 }
 
+
+#' Find children of type
+#'
+#' Small utility like`find_child` but retrieves files by type rather than by specific name.
+#' Returns a vector of ids, with entity names set as names.
+#'
+#' @inheritParams find_child
+#' @param child_type Type(s) as a list, even for only one type. Defaults to "file".
+#' @export
+find_child_type <- function(parent, child_type = list("file")) {
+
+  x <- .syn$getChildren(parent, includeTypes = child_type)
+  y <- reticulate::iterate(x)
+  if(!length(y)) return()
+  z <- setNames(sapply(y, `[[`, "id"), sapply(y, `[[`, "name"))
+  return(z)
+}
+
+
 #' Find data folder
-#' 
+#'
 #' Convenience function to find data folder, which can have slight name variations, in a project.
-#' 
+#'
 #' @param project_id Synapse project id.
 #' @export
 find_data_root <- function(project_id) {
-  
+
   data_root <- find_child("Data", parent = project_id)
   if(is.null(data_root)) data_root <- find_child("Raw Data", parent = project_id)
   data_root
-} 
+}
 
 
 # Find nextflow assets --------------------------------------------------------- #
@@ -60,21 +79,21 @@ find_data_root <- function(project_id) {
 # Convenience functions for getting Synapse ids of nextflow assets
 
 #' Find a standard nextflow workflow output asset
-#' 
+#'
 #' Note that samplesheets became part of the output only for newer versions of nf-core/rna-seq;
-#' older runs may not find samplesheets. 
-#' Paths default to known working paths corresponding to the latest major workflow version, 
-#' but this may change and may need to be updated as part of util maintenance. 
-#' 
+#' older runs may not find samplesheets.
+#' Paths default to known working paths corresponding to the latest major workflow version,
+#' but this may change and may need to be updated as part of util maintenance.
+#'
 #' @param syn_out Id of top-level folder that corresponds to `publishDir` in a nextflow workflow.
 #' @param asset Name of asset to find.
 #' @param workflow Specify workflow, "rna-seq" or "sarek"; defaults to "rna-seq"
-#' @returns Id of samplesheet. 
+#' @returns Id of samplesheet.
 #' @export
-find_nf_asset <- function(syn_out, 
+find_nf_asset <- function(syn_out,
                           asset = c("software_versions", "multiqc_report", "samplesheet", "samtools_stats"),
                           workflow = "rna-seq") {
-  
+
   asset <- match.arg(asset)
   # Assets and paths can differ slightly depending on workflow, except for `software_versions.yml`, get workflow first
   if(workflow == "rna-seq") {
@@ -93,7 +112,7 @@ find_nf_asset <- function(syn_out,
   } else {
     stop("Unrecognized workflow.")
   }
-  
+
   id <- find_in(syn_out, path)
   if(is.null(id)) stop("File not found. Is this the right output directory/path?")
   id
@@ -101,12 +120,12 @@ find_nf_asset <- function(syn_out,
 
 
 #' Return workflow version according to workflow meta
-#' 
+#'
 #' @inheritParams find_nf_asset
-#' @returns Version string. 
+#' @returns Version string.
 #' @export
 nf_workflow_version <- function(syn_out) {
-  
+
   version_meta <- find_nf_asset(syn_out, asset = "software_versions")
   file <- .syn$get(version_meta, downloadFile = TRUE)
   yml <- yaml::read_yaml(file$path)
---
+++ R/remanifest.R
@@ -0,0 +1,108 @@
+#' Reconstitute a manifest
+#'
+#' Quickly "reconstitute" a manifest for files with existing annotations.
+#' This is a *power-user* alternative to schematic's `manifest_generate` with option `useAnnotations = TRUE` for certain scenarios:
+#'
+#' - Meant to be faster and more convenient for workflows focused on scalable/programmatic annotation/revalidation.
+#' Convenience and speed mainly comes from bypassing the schematic API layer and using the Synapse API directly,
+#' so manifest is reconstituted directly as R `data.table` **instead of having to download a Googlesheets or Excel intermediate**.
+#' **The result `.csv` is still intended to be schematic-compatible for validation via `manifest_validate`**.
+#'
+#' - Works with both a Synapse dataset entity as well as a folder;
+#' schematic currently only handles the latter (the folder concept predates Synapse datasets).
+#'
+#' - If folder-scoped, can choose between generating manifest with just immediate files *or*, like schematic, nested files.
+#' This is useful, for example, if the main scope folder is "RNA-seq" and the main files are fastqs, but there is a child folder called "md5" with md5 files,
+#' and the manifest is really meant for the fastqs.
+#'
+#' - Does not try to download `synapse_manifest.csv`, which can be useful to regenerate a manifest when ARs have been set.
+#'
+#' - Does not require an asset view.
+#'
+#' Other developer notes:
+#' See also `gather_annotations` for the under-the-hood workhorse.
+#' Intended for further development to be used as part of advanced annotation utils.
+#'
+#' @param scope Synapse id of something representing one or more datasets. Can be a folder *or* Synapse dataset entity.
+#' @param file Name of file to be written. Use `NULL` to return the `data.table` instead of writing to file.
+#' @param recurse Include everything nested within folder scope, default `TRUE`, or only the immediate files.
+#' Not used if dataset is Synapse dataset entity.
+#' @param mf Intermediate manifest to build upon; used when recursive.
+#' @import data.table
+#' @export
+remanifest <- function(scope,
+                       file = "manifest.csv",
+                       recurse = TRUE,
+                       mf = data.table()) {
+
+  dataset <- .syn$get(scope[1])
+  if(dataset$properties$concreteType == "org.sagebionetworks.repo.model.table.Dataset") {
+
+    dataset_items <- sapply(dataset$properties$datasetItems, `[[`, "entityId")
+    if(!length(dataset_items)) {
+
+      return(data.table())
+
+    } else {
+
+      manifest <- gather_annotations(dataset_items)
+      if(!is.null(file)) save_manifest(manifest, file) else manifest
+
+    }
+
+  } else if(dataset$properties$concreteType == "org.sagebionetworks.repo.model.Folder") {
+
+    files <- find_child_type(scope[1])
+    if(length(files)) {
+      ids <- files
+      manifest <- gather_annotations(ids)
+    } else {
+      manifest <- data.table()
+    }
+
+    if(recurse) {
+      more <- find_child_type(scope[1], list("folder"))
+      scope <- c(scope[-1], more)
+      if(!length(scope)) {
+        manifest <- rbind(mf, manifest, fill = T)
+        if(!is.null(file)) save_manifest(manifest, file) else manifest
+      } else {
+        remanifest(scope = scope,
+                   recurse = recurse,
+                   mf = rbind(mf, manifest, fill = T))
+      }
+    } else {
+      if(!is.null(file)) save_manifest(manifest, file) else manifest
+    }
+
+  } else {
+
+    "Dataset is only accepted as a folder or dataset entity. Please check the id used."
+
+  }
+}
+
+#' Internal helper for gathering annotations into a table using the REST API
+#'
+#' This is an internal implementation that works directly with the platform service JSON
+#' to afford more low-level control and avoid Python-R object conversion differences between reticulate versions.
+#'
+#' @param ids One or more ids.
+#' @param list_sep List separator for list annotations.
+#' @keywords internal
+gather_annotations <- function(ids, list_sep = ", ") {
+  results <- lapply(ids, function(id) .syn$restGET(glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/entity/{id}/annotations2")))
+  annotations <- lapply(results, function(x) c(list(entityId = list(value = x$id)), x$annotations))
+  annotations <- lapply(annotations, function(x) lapply(x, function(col) paste(col$value, collapse = list_sep)))
+  manifest <- data.table::rbindlist(annotations, fill = TRUE)
+  manifest
+}
+
+
+#' Save manifest
+#'
+#' @keywords internal
+save_manifest <- function(manifest, file) {
+  fwrite(manifest, file = file)
+  checked_message(glue::glue("Saved manifest as {file}"))
+}
