cebe432af50a5947d3b870bfce3c6329d448f025
Anh Nguyet Vu <anngvu@gmail.com>
2024-02-23 09:59:28 -0700

    Update annotations.R

--- R/annotations.R
+++ R/annotations.R
@@ -1,16 +1,16 @@
 #' Set annotations from a manifest
-#' 
-#' The [Synapse docs](https://help.synapse.org/docs/Managing-Custom-Metadata-at-Scale.2004254976.html) 
-#' suggest doing batch annotations through a fileview. However, it is often simpler to 
-#' modify or set new annotations directly given a table of just the entities (rows) and props (cols) we want. 
+#'
+#' The [Synapse docs](https://help.synapse.org/docs/Managing-Custom-Metadata-at-Scale.2004254976.html)
+#' suggest doing batch annotations through a fileview. However, it is often simpler to
+#' modify or set new annotations directly given a table of just the entities (rows) and props (cols) we want.
 #' This is like how schematic works, except without any validation (so works best for power-users who know the data model well).
-#' Some desired defaults are taken into account, such as not submitting key-values with `NA` and empty strings. 
-#' 
+#' Some desired defaults are taken into account, such as not submitting key-values with `NA` and empty strings.
+#'
 #' @param manifest A table manifest. Needs to contain `entityId`.
 #' @param ignore_na Whether to ignore annotations that are `NA`; default TRUE.
 #' @param ignore_blank Whether to ignore annotations that are that empty strings; default TRUE.
 #' @param verbose Be chatty, default FALSE.
-#' @export   
+#' @export
 annotate_with_manifest <- function(manifest, ignore_na = TRUE, ignore_blank = TRUE, verbose = FALSE) {
   # Split by `entityId`
   annotations <- as.data.table(manifest)
@@ -21,31 +21,29 @@ annotate_with_manifest <- function(manifest, ignore_na = TRUE, ignore_blank = TR
   filterBlank <- if(ignore_blank) function(x) !any(x == "") else TRUE # same as above
   annotations <- lapply(annotations, function(x) Filter(function(x) filterNA(x) & filterBlank(x) & length(x), unlist(x, recursive = F)))
   for(entity in names(annotations)) {
-    .syn$setAnnotations(entity = entity, annotations = as.list(annotations[[entity]]))
+    synapser::synSetAnnotations(entity = entity, annotations = as.list(annotations[[entity]]))
   }
   if (verbose) message("Annotations submitted")
 }
 
 
 #' Copy annotations
-#' 
+#'
 #' Copy annotations (all or selectively) from a source entity to one or more target entities.
-#' If annotations already exist on target entities, the copy will replace the current values. 
-#' 
-#' @param entity_from Syn id from which to copy. 
-#' @param entity_to One or more syn ids to copy annotations to. 
-#' @param select Vector of properties to selectively copy if present on the entity. 
+#' If annotations already exist on target entities, the copy will replace the current values.
+#'
+#' @param entity_from Syn id from which to copy.
+#' @param entity_to One or more syn ids to copy annotations to.
+#' @param select Vector of properties to selectively copy if present on the entity.
 #' If not specified, will copy over everything, which may not be desirable.
-#' @param update Whether to immediately update or return annotation objects only. 
+#' @param update Whether to immediately update or return annotation objects only.
 #' @export
 copy_annotations <- function(entity_from,
                              entity_to,
                              select = NULL,
                              update = FALSE) {
-  
-  .check_login()
-  
-  annotations <- .syn$get_annotations(entity_from)
+
+  annotations <- synapser::synGetAnnotations(entity_from)
   if(is.null(select)) {
     cp <- annotations
   } else {
@@ -54,10 +52,10 @@ copy_annotations <- function(entity_from,
       if(k %in% select) cp[k] <- annotations[k]
     }
   }
-  
+
   if(update) {
     for(e in entity_to) {
-      .syn$setAnnotations(e, annotations = cp)
+      synapser::synSetAnnotations(e, annotations = cp)
     }
   } else {
     return(cp)
