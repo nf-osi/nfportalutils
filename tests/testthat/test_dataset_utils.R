be7e67d58352cc754cfa38674bcf29f6302e4060
Anh Nguyet Vu <anngvu@gmail.com>
2024-02-23 10:03:20 -0700

    Update tests

--- tests/testthat/test_dataset_utils.R
+++ tests/testthat/test_dataset_utils.R
@@ -1,6 +1,8 @@
-# Create a basic draft dataset from some files at version 1; all files have a latest version 2 
+# Create a basic draft dataset from some files at version 1; all files have a latest version 2
 # Returns dataset id only
 create_dataset_fixture <- function(instance = 1) {
+
+  skip_if_no_login()
   NF_test <- "syn26462036"
   items <- c("syn51239179",
              "syn51239178",
@@ -12,9 +14,9 @@ create_dataset_fixture <- function(instance = 1) {
 
 
 test_that("Creating dataset with `new_dataset` works as expected when given valid parameters, defaulting to current item versions", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
+
+  skip_if_no_login()
+
   NF_test <- "syn26462036"
   # Note that files are all version 2 on Synapse
   items <- c("syn51239179",
@@ -24,16 +26,16 @@ test_that("Creating dataset with `new_dataset` works as expected when given vali
   expected_items_in_dataset <- list(
     list(entityId = "syn51239179", versionNumber = 2L),
     list(entityId = "syn51239178", versionNumber = 2L),
-    list(entityId = "syn51239177", versionNumber = 2L)) 
+    list(entityId = "syn51239177", versionNumber = 2L))
   testthat::expect_equal(expected_items_in_dataset, dataset$properties$datasetItems)
-  .syn$delete(dataset)
+  synapser::synDelete(dataset)
 })
 
 
 test_that("Creating dataset with `new_dataset` works as expected when given valid parameters and a specific item version is specified", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
+
+  skip_if_no_login()
+
   NF_test <- "syn26462036"
   items <- c("syn51239179",
              "syn51239178",
@@ -44,7 +46,7 @@ test_that("Creating dataset with `new_dataset` works as expected when given vali
     list(entityId = "syn51239178", versionNumber = 1L),
     list(entityId = "syn51239177", versionNumber = 1L))
   testthat::expect_equal(expected_items_in_dataset, dataset$properties$datasetItems)
-  .syn$delete(dataset)
+  synapser::synDelete(dataset)
 })
 
 
@@ -54,9 +56,9 @@ test_that("Creating dataset with `new_dataset` works as expected when given vali
 # Currently, only files can be included in a dataset. syn27242487 is 'org.sagebionetworks.repo.model.table.TableEntity'
 # ```
 test_that("Creating dataset with `new_dataset` will fail when trying to include a non-valid item (a table)", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
+
+  skip_if_no_login()
+
   NF_test <- "syn26462036"
   items <- c("syn51239179",
              "syn51239178",
@@ -66,10 +68,9 @@ test_that("Creating dataset with `new_dataset` will fail when trying to include 
 
 
 test_that("Updating a dataset to make a subset of files reference the latest version works", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
-  
+
+  skip_if_no_login()
+
   dataset_id <- create_dataset_fixture()
   items_to_update <- c("syn51239178", "syn51239177") # both should be updated to Version 2
   updated <- use_latest_in_collection(collection_id = dataset_id, items = items_to_update)
@@ -78,15 +79,14 @@ test_that("Updating a dataset to make a subset of files reference the latest ver
     list(entityId = "syn51239178", versionNumber = 2L),
     list(entityId = "syn51239177", versionNumber = 2L))
   testthat::expect_identical(updated$items, expected_updated_items)
-  .syn$delete(dataset_id)
+  synapser::synDelete(dataset_id)
 })
 
 
 test_that("Updating a dataset to make _all_ files reference the latest version works", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
-  
+
+  skip_if_no_login()
+
   dataset_id <- create_dataset_fixture()
   expected_updated_items <- list(
     list(entityId = "syn51239179", versionNumber = 2L),
@@ -94,17 +94,16 @@ test_that("Updating a dataset to make _all_ files reference the latest version w
     list(entityId = "syn51239177", versionNumber = 2L))
   updated <- use_latest_in_collection(collection_id = dataset_id, items = "all")
   testthat::expect_identical(updated$items, expected_updated_items)
-  .syn$delete(dataset_id)
+  synapser::synDelete(dataset_id)
 })
 
 
 # Dataset collections ---------------------------------------------------------#
 
 test_that("Updating a dataset collection to make a subset of datasets reference the latest version works", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
-  
+
+  skip_if_no_login()
+
   dataset_collection_id <- "syn51809938"
   dataset_item_to_update <- "syn51809898"
   .syn$create_snapshot_version(dataset_item_to_update)
@@ -117,9 +116,8 @@ test_that("Updating a dataset collection to make a subset of datasets reference 
 
 test_that("Adding new dataset to dataset collection works", {
 
-  skip_if_no_synapseclient()
-  skip_if_no_token()
-  
+  skip_if_no_login()
+
   dataset_collection_id <- "syn51809938"
   coll_state <- coll <- .syn$restGET(glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/entity/{dataset_collection_id}"))
   one_more_item <- create_dataset_fixture()
@@ -127,17 +125,16 @@ test_that("Adding new dataset to dataset collection works", {
   testthat::expect_equal(length(new_coll_state$items), length(coll_state$items) + 1L)
   # cleanup: set collection to previous items state
   new_coll_state$items <- coll_state$items
-  .syn$restPUT(glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/entity/{dataset_collection_id}"), body = jsonlite::toJSON(new_coll_state, auto_unbox = TRUE))
+  synapser::synRestPUT(glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/entity/{dataset_collection_id}"), body = jsonlite::toJSON(new_coll_state, auto_unbox = TRUE))
   # delete dataset
-  .syn$delete(one_more_item)
+  synapser::synDelete(one_more_item)
 })
 
 
 test_that("Adding non-datasets to dataset collection gives expected handling and warning", {
-  
-  skip_if_no_synapseclient()
-  skip_if_no_token()
-  
+
+  skip_if_no_login()
+
   dataset_collection_id <- "syn51809938"
   bad_items <- "syn51106349" # a folder
   testthat::expect_warning(add_to_collection(collection_id = dataset_collection_id, items = bad_items, check_items = TRUE),
