ac2193799d5bde8be4492099fe088e625f3e5c8c
Anh Nguyet Vu <anngvu@gmail.com>
2026-04-14 15:14:19 -0700

    Remove no longer relevant tests

--- tests/testthat/test_new_project_strict.R
+++
@@ -1,5 +0,0 @@
-test_that("new_project_strict fails if clash with existing project", {
-  skip_if_no_synapseclient() # does not require pandas
-  skip_if_no_login()
-  expect_error(new_project_strict("My project"))
-})
